b3d4a7899f6a35e18125935a792dba8364f38f7b
samara caldas <samara.ferreira@bnpsolucoes.com.br>
2026-02-25 16:58:26 -0300

    feat: enhance CORS configuration to allow specific origins for improved security

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,8 +14,21 @@ import accountRoutes from "./routes/account.routes";
 connectDB();
 
 const app = express();
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://financeflow-orcin.vercel.app',
+  ]
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  }))
 app.use(express.json())
 app.use(cookieParser())
 
